f266cf8bd60fbf9a88a46be0562b7b3bc7733bb9
Nishant Kyal <nishant.kyal@gmail.com>
2026-03-11 18:21:27 +0530

    added google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -66,6 +66,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+          <Script id="gtm" strategy="lazyOnload">
+            {`(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-KRGPDT77');`}
+          </Script>
+
         {/* Google Tag Manager */}
         <Script id="gtm" strategy="lazyOnload">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
